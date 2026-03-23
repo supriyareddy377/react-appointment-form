fa86e9ee32c021af9fe5fc298f075bf68efd59a8
supriyareddy377 <supriyareddy377@gmail.com>
2026-03-23 12:49:44 +0530

    mail functionality modified

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -6,8 +6,8 @@ const sendEmail = async (name, phone, email, message) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
