cc4ab1adbd20f4354b5bac0a9cef58dbe81230fd
supriyareddy377 <supriyareddy377@gmail.com>
2026-03-23 12:58:03 +0530

    mail functionality revert to old

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -6,8 +6,8 @@ const sendEmail = async (name, phone, email, message) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
